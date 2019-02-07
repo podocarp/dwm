4616eebb7abe2eeae5ce1a5c9ea8fe039290b23e
jxd <xdjiaxd@gmail.com>
2019-02-07 19:40:10 +0800

    changed dmenu to j4 dmenu

--- config.h
+++ config.h
@@ -68,7 +68,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_white, "-sf", col_black, NULL };
+//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_white, "-sf", col_black, NULL };
+static const char *dmenucmd[] = {"j4-dmenu-desktop"};
 static const char *termcmd[]  = { "tabbed", "st", "-w", NULL };
 
 static Key keys[] = {
