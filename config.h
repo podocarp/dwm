1b831824ead581ce35fb218e503adc307d67f8b6
jxd <xdjiaxd@gmail.com>
2019-02-09 13:05:38 +0800

    new colors and fonts

--- config.h
+++ config.h
@@ -1,25 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 4;
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 5;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=11" };
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = {
+    "IBM Plex Sans Text:size=11",
+    "monospace:size=11",
+};
 static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#111111";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#999999";
-static const char col_gray4[]       = "#CCCCCC";
+static const char col_gray3[]       = "#666666";
+static const char col_gray4[]       = "#999999";
 static const char col_black[]	    = "#000000";
 static const char col_white[]       = "#FFFFFF";
-#define baralpha 0xD8
+#define baralpha 0xC0
 #define borderalpha OPAQUE
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_black, col_gray3, col_black },
-	[SchemeSel]  = { col_black, col_gray4,  col_white  },
+	[SchemeSel]  = { col_black, col_gray4, col_white }
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
