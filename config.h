382eb694f25f343e218b421d72cdaea37d9d88f0
Xiaodong <xdjiaxd@gmail.com>
2019-10-20 16:58:13 +0800

    added default gaps to make borders obvious

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 0;
+static const unsigned int gappx     = 5;
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -47,7 +47,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.635; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.375; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
