d87b77e9775beb9ec28e3d90b1822c89e7ab37ee
jxd <xdjiaxd@gmail.com>
2019-10-21 16:08:28 +0800

    added another rule for firefox

--- config.h
+++ config.h
@@ -44,6 +44,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Nightly",  "Navigator",NULL,       1 << 8,       0,           -1 },
+	{ "Firefox Developer Edition",  "Navigator",NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
